67f01cceb48e2b15aeb88b6f42df49ca43535e97
Shrikant Ghuge <shreeghuge1@gmail.com>
2022-06-24 11:15:06 +0530

    added outstanding module

---
+++ src/app/components/outstanding-details/outstanding-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outstanding-details',
+  templateUrl: './outstanding-details.component.html',
+  styleUrls: ['./outstanding-details.component.scss']
+})
+export class OutstandingDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
