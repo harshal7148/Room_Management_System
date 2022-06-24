a658e51ce450e1c5abdc9d37cc52265239016ab2
Shrikant Ghuge <shreeghuge1@gmail.com>
2022-06-24 11:15:27 +0530

    added outstanding module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TenantsComponent } from './components/tenants/tenants.component';
+import { OutstandingDetailsComponent } from './components/outstanding-details/outstanding-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TenantsComponent
+    TenantsComponent,
+    OutstandingDetailsComponent
   ],
   imports: [
     BrowserModule,
